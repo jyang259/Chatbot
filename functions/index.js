c43027b1a22107fb0966249ffb446fd3201f3d35
Jenny Yang <yangjy259@gmail.com>
2018-05-14 12:54:20 -0700

    new functions test

--- functions/index.js
+++ functions/index.js
@@ -11,8 +11,14 @@ const NAME_ACTION = 'ask_name';
 const GIVENNAME_ARGUMENT = 'givenname';
 const LASTNAME_ARGUMENT = 'lastname';
 
+// a. the action name from the ask_birthdate Diaglogflow intent
+const BIRTHDATE_ACTION = 'ask_birthdate';
+// b. the parameters that are parsed from the ask_birthdate intent
+const BIRTHDATE_ARGUMENT = 'birthdate';
 
-exports.askName = functions.https.onRequest((request, response) => {
+//Start the action
+//ask name
+exports.askInfo = functions.https.onRequest((request, response) => {
   const app = new App({request, response});
   console.log('Request headers: ' + JSON.stringify(request.headers));
   console.log('Request body: ' + JSON.stringify(request.body));
@@ -26,6 +32,25 @@ exports.askName = functions.https.onRequest((request, response) => {
       givenname + ' ' + lastname +
       '! Let\'s keep going!');
   }
+
+// c. The function that gets the name
+  function getBirthdate (app) {
+    let birthdate = app.getArgument(BIRTHDATE_ARGUMENT);
+    app.tell('Okay, your date of birth is saved as ' +
+      birthdate);
+  }
+
+// // Return the last journal entry
+//   function reflect (app) {
+//     let grateful_1 = app.getArgument(GRATEFUL_1);
+//     app.tell('Here is your previous entry: ' + grateful_1);
+//   }
+
+  // // Build an action map, which maps intent names to functions
+  // let actionMap = new Map();
+  // actionMap.set(QUESTION_1, reflect);
+
+
   // d. build an action map, which maps intent names to functions
   let actionMap = new Map();
   actionMap.set(NAME_ACTION, getName);
@@ -33,3 +58,19 @@ exports.askName = functions.https.onRequest((request, response) => {
 
 app.handleRequest(actionMap);
 });
+
+// //ask birthdate
+// exports.askBirthdate = functions.https.onRequest((request, response) => {
+//   const app = new App({request, response});
+//   console.log('Request headers: ' + JSON.stringify(request.headers));
+//   console.log('Request body: ' + JSON.stringify(request.body));
+
+
+
+//   // d. build an action map, which maps intent names to functions
+//   let actionMap = new Map();
+//   actionMap.set(BIRTHDATE_ACTION, getBirthdate);
+
+
+// app.handleRequest(actionMap);
+// });
